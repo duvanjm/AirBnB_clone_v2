af3621a13b99417a91493447734a99b8fba26e7e
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-11 20:15:51 +0000

    ALL PRIVILEGES changed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,8 +5,8 @@ USE hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS ‘hbnb_dev’@'localhost' IDENTIFIED BY 'hbnt_dev_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev' @'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 GRANT
 SELECT
-    ON performance_schema.* TO 'hbnb_dev' @'localhost';
+    ON performance_schema.* TO 'hbnb_dev'@'localhost';
