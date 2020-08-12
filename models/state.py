c7613da1ace21bdb9f5a6e1c7977e68cf50518e5
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-12 20:48:54 +0000

    validate HBNB_TYPE_STORAGE

--- models/state.py
+++ models/state.py
@@ -5,12 +5,17 @@ from models.base_model import Base
 from sqlalchemy import Colum
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Colum(
-        String(128),
-        nullable=False
-    )
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Colum(
+            String(128),
+            nullable=False
+        )
+    else:
+        name = ''
