19d86a1f64a1c17f110188ac90fed79ecc8aee9f
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-12 19:58:11 +0000

    models/state.py added.

--- models/state.py
+++ models/state.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Colum
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Colum(
+        String(128),
+        nullable=False
+    )
