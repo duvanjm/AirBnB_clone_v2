443f49dbcbd5e4b2f89f0db445982171711bc98a
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-12 20:48:51 +0000

    validate HBNB_TYPE_STORAGE

--- models/__init__.py
+++ models/__init__.py
@@ -6,17 +6,17 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-import os
+from os import getenv
 
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
 
 
 classes = {
