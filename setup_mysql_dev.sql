a0acd5cc355e1073f29d7481c96cd56bba8315ae
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-13 01:27:15 +0000

    setup_mysql_dev.sql added.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,17 @@
 -- script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE USER IF NOT EXISTS hbnb_dev @localhost IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost';
 
-GRANT
-USAGE
-    ON *.*
-    TO 'hbnb_dev' @'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+SET
+    PASSWORD FOR 'hbnb_dev' @'localhost' = PASSWORD('hbnb_dev_pwd');
 
-GRANT
-ALL PRIVILEGES
-    ON hbnb_dev_db.*
-    TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_dev' @'localhost';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
 
 GRANT
 SELECT
-    ON performance_schema.* TO 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+    ON performance_schema.* TO 'hbnb_dev' @'localhost';
+
+FLUSH PRIVILEGES;
