2d1a9c488a2d4c1072aeb0898ff08c527f4078bd
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-13 16:11:06 +0000

    console fixed

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
@@ -9,8 +10,9 @@ from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
         name = Column(
             String(128),
             nullable=False
