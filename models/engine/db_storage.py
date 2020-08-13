895fca30e482b6c922664ba8f9691798d55c4de8
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-13 16:24:32 +0000

    test for task 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -33,7 +33,7 @@ class DBStorage:
     def all(self, cls=None):
         """ query on the current database session - optional, filter by cls"""
 
-        d = {}
+        """ d = {}
 
         if cls:
             obj = self.__session.query(cls).all()
@@ -46,9 +46,9 @@ class DBStorage:
         for item in obj:
             k = type(item).__name__ + '.' + str(item.id)
             d[k] = item
-        return d
+        return d """
 
-        """ dict_ = {}
+        dict_ = {}
 
         if cls:
             query = self.__session.query(cls).all()
@@ -61,7 +61,7 @@ class DBStorage:
                 for obj in query:
                     delattr(obj, '_sa_instance_state')
                     dict_[obj.__class__.__name__ + '.' + obj.id] = obj
-        return dict_ """
+        return dict_
 
     def new(self, obj):
         """ add the object to the current database session """
