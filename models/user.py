81f1788b9939774447e92854ddcc72bbaf68ad71
duvanjm <duvanjarin@gmail.com>
2020-08-13 19:23:52 +0000

    fix user.py

--- models/user.py
+++ models/user.py
@@ -17,7 +17,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
