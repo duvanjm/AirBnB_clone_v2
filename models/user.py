598aeddcc872505dc3cb61e3c23e371154bde2ef
duvanjm <duvanjarin@gmail.com>
2020-08-13 19:25:34 +0000

    fix user.py

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = "user"
-    if getenv("HBTN_TYPE_STORAGE") == "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
