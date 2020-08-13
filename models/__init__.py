ed2bbac3ed5baa1deb0ecd5a1c34c87595dc8fc1
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-13 01:27:15 +0000

    models/__init__.py added.

--- models/__init__.py
+++ models/__init__.py
@@ -4,14 +4,13 @@ from os import getenv
 from models.user import User
 from models.place import Place
 from models.state import State
-""" from models.city import City
-from models.amenity import Amenity
+from models.city import City
+""" from models.amenity import Amenity
 from models.review import Review """
 
 classes = {
-    'User': User,
-    'Place': Place,
     'State': State,
+    'City': City
 }
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
