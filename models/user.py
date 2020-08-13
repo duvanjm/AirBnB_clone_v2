45e73d52e8dc8515d881ca5e47d538d20f29ac12
Santiago Agudelo <sagudelo1200@gmail.com>
2020-08-13 20:48:36 +0000

    nullable removed in finrst and last name

--- models/user.py
+++ models/user.py
@@ -16,8 +16,8 @@ class User(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
         places = relationship('Place', backref='user')
     else:
         email = ''
