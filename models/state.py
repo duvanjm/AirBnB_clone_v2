46617f65ec803cdf54f919a8e2ea00ef30e8144e
Duvan <duvanjarin@gmail.com>
2020-09-02 11:54:34 -0500

    fix task 7

--- models/state.py
+++ models/state.py
@@ -2,10 +2,12 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
+from models.city import City
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
 from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -25,8 +27,8 @@ class State(BaseModel, Base):
         """return the list of City objects
         from storage linked to the current State"""
         list_ = []
-        sta = storage.all(City)
+        sta = models.storage.all(City)
         for k, v in sta:
-            if self.id == v.state_id:
+            if v.state_id == self.id:
                 list_.append(v)
         return list_
