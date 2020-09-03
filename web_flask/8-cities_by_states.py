c32d407bd16f3795bd12e7471c6948bff95d1448
duvanjm <duvanjarin@gmail.com>
2020-09-03 00:00:18 +0000

    fix task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-from flask import Flask
-from flask import render_template
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
@@ -10,26 +9,26 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(self):
-    """method to handle"""
+    """reload close files storage"""
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def state_l():
-    """display a HTML page"""
-    state = storage.all(State).values()
-    return render_template('7-states_list.html', states=state)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page: (inside the tag BODY)"""
+    context = storage.all(State).values()
+    return render_template('7-states_list.html', states=context)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities():
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
     """display a HTML page"""
     states_city = {
         'states': storage.all(State).values(),
         'cities': storage.all(City).values()
     }
+    return render_template("8-cities_by_states.html", **states_city)
 
-    return render_template('8-cities_by_states.html', **states_city)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
